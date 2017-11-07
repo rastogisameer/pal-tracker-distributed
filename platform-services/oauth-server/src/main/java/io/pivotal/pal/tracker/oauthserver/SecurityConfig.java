631f72606ecb251c855eb7651b61e86b405f53b5
Sameer Rastogi <sameer.rastogi@gmail.com>
2017-11-07 17:20:07 +0530

    oauth

--- platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/SecurityConfig.java
+++ platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/SecurityConfig.java
@@ -4,17 +4,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class SecurityConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
-    public void configure(ClientDetailsServiceConfigurer clients)throws Exception {
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-        .withClient("tracker-client")
-        .secret("supersecret")
-        .authorizedGrantTypes("client_credentials")
-        .scopes("openid");
+                .withClient("tracker-client")
+                .secret("supersecret")
+                .authorizedGrantTypes("client_credentials")
+                .scopes("openid");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer)
+            throws Exception {
+        oauthServer
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
     }
 }
